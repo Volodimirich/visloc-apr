eb790934152b7eec57b050e6e2e818ee38253b57
PolevanovaIrina <36921411+PolevanovaIrina@users.noreply.github.com>
2021-10-26 22:05:18 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 
-setup(setup_requires=['visdom', 'pytorch', 'torchvision', 'jupyter', 'matplotlib', 'transforms3d'],
-      install_requires=['visdom', 'pytorch', 'torchvision', 'jupyter', 'matplotlib', 'transforms3d'])
+setup(setup_requires=['visdom', 'torch', 'torchvision', 'jupyter', 'matplotlib', 'transforms3d'],
+      install_requires=['visdom', 'torch', 'torchvision', 'jupyter', 'matplotlib', 'transforms3d'])
