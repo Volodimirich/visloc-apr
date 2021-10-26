b4f00227a23d1f15e79261550c8fe0d54dc08df5
Matvey Volkov <m.b.volkov@yandex.ru>
2021-10-26 23:06:08 +0300

    Update download.sh

--- download.sh
+++ download.sh
@@ -2,7 +2,7 @@
 
 mkdir data
 wget - './data' --content-disposition http://140.238.217.97/index.php/s/VfnQ6TanxzBnpcP/download
-unzip ./data/CambridgeLandmarks.zip -d ./data/
+unzip ./CambridgeLandmarks.zip -d ./data/
 
 
 wget -P './weights' https://vision.in.tum.de/webshare/u/zhouq/visloc-apr/models/googlenet_places.extract.pth
